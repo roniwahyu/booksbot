971846962af16d4273c93e9e375a457a355d8af1
Roniwahyu <roniwahyu@gmail.com>
2019-11-11 22:43:15 +0700

    update

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -14,15 +14,15 @@ class BooksSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product-display > h3 > a ::attr(href)").extract():
+        for book_url in response.css("div.o-layout__item.product-description > h3 > a ::attr(href)").extract():
             yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
+        next_page = response.css("a.next_page ::attr(href)").extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
     def parse_book_page(self, response):
         item = {}
-        product = response.css("div.o-layout__item")
+        product = response.css("div.o-layout__item.u-8of12.u-position-relative")
         item["title"] = product.css("h1 ::text").extract_first()
         # item['category'] = response.xpath(
         #     "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
